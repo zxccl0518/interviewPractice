a713bb6009ed7071078312b199f004e709d19389
zxccl0518 <515361971@qq.com>
2019-07-17 23:59:23 +0800

    2019-07-17

--- http/main.go
+++ http/main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/hello", func(w http.ResponseWriter, h *http.Request) {
-		w.Write([]byte("hhhhhhhhhhhhhhhhhhhhhhhhhhhh"))
+		w.Write([]byte(""))
 	})
 
 	http.ListenAndServe("127.0.0.1:8080", nil)
