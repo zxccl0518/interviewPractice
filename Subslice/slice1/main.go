42cfcaf5dff5616cedd661a3d906544905ecd277
jackie.zhao <jackie.zhao@robo-sys.com>
2019-08-09 15:31:42 +0800

    2019-08-09 15:31  更新了几道 面试相关的内容

--- Subslice/slice1/main.go
+++ Subslice/slice1/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"bytes"
+	"container/heap"
+	"context"
 	"fmt"
 	"runtime"
+	"sort"
+	"strings"
 	"sync"
+	"time"
+	"unicode/utf8"
 )
 
 func sliceTest1() {
@@ -135,6 +142,8 @@ func InterfaceTest2() {
 
 //-----------------------------------------------------------
 // Add code in line A to assure that the lowercase letters and capital letters are printed consecutively.
+// 看明白了 这个 Gosched()这个函数的意思了. 就是吧当前的cpu的执行权让出来,但是不会将goroutine挂起.
+// 让出执行权的goroutine 会自动被恢复的.
 func GoSchedTest() {
 	const GOMAXPROCS = 1
 	const N = 26
@@ -146,6 +155,7 @@ func GoSchedTest() {
 		go func(i int) {
 			defer wg.Done()
 			// A
+			runtime.Gosched()
 			fmt.Printf("%c", 'a'+i)
 		}(i)
 		go func(i int) {
@@ -189,6 +199,145 @@ func mutexTest() {
 }
 
 //-----------------------------------------------------------
+// Map Immutability
+// 没太明白 这个map 的原理.
+type mapS struct {
+	name string
+}
+
+func MapTest() {
+	//cannot assign to struct field m["x"].name in map
+	m := map[string]mapS{"x": mapS{"one"}}
+	// m["x"].name = "two"
+
+	// 正确写法,
+	// m := map[string]*mapS{"x": &mapS{"one"}}
+	// m["x"].name = "two"
+	fmt.Println("m = ", m)
+}
+
+//-----------------------------------------------------------
+//Add code to line A to sort s in ascending order
+// 考点是 对于slice 的排序, 可以用 sort.Slice()这个方法进行排序.
+// sort.SliceStable() 这个方法是相对于sort.Slice() 稳定的方法..
+type sliceS struct {
+	v int
+}
+
+func SliceSortingTest() {
+	s := []sliceS{{1}, {3}, {5}, {2}}
+	//A
+	sort.SliceStable(s, func(i, j int) bool { return s[i].v < s[j].v })
+	fmt.Printf("%#v ", s)
+}
+
+//-----------------------------------------------------------
+// utf8 length
+//Fix the mistake below to assure the length of utf8 string can be printed correctly.
+func Utf8Test() {
+	// len() 返回的是字节数.  一个汉字3个字节.
+	fmt.Println(len("你好"))
+
+	// 若想返回的是直接的 汉字的个数 可以 用 utf8.RuneCountString()方法.
+	fmt.Println(utf8.RuneCountInString("你好"))
+	fmt.Println(utf8.RuneCountInString("hello"))
+
+	fmt.Println(strings.IndexRune("helloworld", 'l'))
+	fmt.Println(strings.IndexRune("今晚打老虎", '打'))
+
+	fmt.Println("bytes.Count() = ", bytes.Count([]byte("abc123今晚打老虎"), []byte("打")))
+	fmt.Println("utf8.RuneCount() = ", utf8.RuneCount([]byte("abc123今晚打老虎")))
+	fmt.Println("strings.Count() = ", strings.Count("abc123今晚打老虎", "打"))
+}
+
+//-----------------------------------------------------------
+// What will be printed when the code below is executed?
+func HeapTest() {
+	h := &IntHeap{2, 1, 5}
+	heap.Init(h)
+	for h.Len() > 0 {
+		fmt.Printf("%d ", heap.Pop(h))
+	}
+}
+
+type IntHeap []int
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+//-----------------------------------------------------------
+//What will be printed when the code below is executed?
+// 结论是 不会等到3秒的时候 在让ctx接收到 done（）的信号， 等待1秒的时候就放回了。
+func contextWithTimeOut() {
+	timeout := 3 * time.Second
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	select {
+	case <-time.After(1 * time.Second):
+		fmt.Println("waited  for 1 sec")
+	case <-time.After(1 * time.Second):
+		fmt.Println("waited  for 2 sec")
+	case <-time.After(1 * time.Second):
+		fmt.Println("waited  for 3 sec")
+	case <-ctx.Done():
+		fmt.Println(ctx.Err())
+	}
+}
+
+//-----------------------------------------------------------
+// append之后 扩容了， 原来底层维护的数组不再使用。所以append之后的slice 地址就变了。
+func sliceAddress() {
+	slice1 := make([]int, 10)
+	fmt.Printf("地址 ：%p\n", slice1)
+	for v := range slice1 {
+		fmt.Println("v = ", v)
+	}
+
+	slice1 = append(slice1, 1)
+	fmt.Printf("地址 ：%p\n", slice1)
+
+	var i = -26
+	fmt.Printf("i = %08b\n", i)
+	fmt.Println("i = ", i)
+	i = i >> 4
+	fmt.Printf("i = %08b\n", i)
+	fmt.Println("i = ", i)
+
+}
+
+//-----------------------------------------------------------
+//
+// https://blog.csdn.net/wowzai/article/details/8941865
+func UnicodeIndex(str, substr string) int {
+	// 子串在字符串的字节位置
+	result := strings.Index(str, substr)
+	if result >= 0 {
+		// 获得子串之前的字符串并转换成[]byte
+		prefix := []byte(str)[0:result]
+		// 将子串之前的字符串转换成[]rune
+		rs := []rune(string(prefix))
+		// 获得子串之前的字符串的长度，便是子串在字符串的字符位置
+		result = len(rs)
+	}
+
+	return result
+}
+
+//-----------------------------------------------------------
 
 func main() {
 	// sliceTest1()
@@ -201,5 +350,20 @@ func main() {
 
 	// mutexTest()
 
-	GoSchedTest()
+	// GoSchedTest()
+
+	// MapTest()
+
+	// SliceSortingTest()
+
+	Utf8Test()
+
+	// 应该是测试 go heap堆得一个 包里面的方法。
+	// HeapTest()
+
+	// contextWithTimeOut()
+
+	// sliceAddress()
+
+	// fmt.Println(UnicodeIndex("abc123今晚打老虎", "打"))
 }
